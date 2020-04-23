f4d20e75cf46b8bab142721ad3c4dfd8b0a377d0
Filipe Maciel <devfilsk@gmail.com>
2020-04-23 14:54:48 -0300

    Criando base de autenticação com typescript, hook customizado e useContext

---
+++ README.md
@@ -0,0 +1,10 @@
+## Criando todo fluxo de autenticação com react-native
+
+### Tecnologias utilizadas:
+* react-navigation v5
+* axios
+* react hook
+* typescript
+* hook customizado para contexto da autenticação
+* exibição de stack de rotas a partir da autenticação ou não
+* loading global para autenticação
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
+
+import { signIn } from '../../services/auth';
+import { useAuth } from '../../contexts/auth';
+
+const styles = StyleSheet.create({
+    container: { flex: 1, justifyContent: 'center'}
+})
+
+const SignIn: React.FC = () => {
+    const { signed, user, signIn } = useAuth();
+
+    async function handleSignIn() {
+        signIn();   
+    }
+    return (
+        <View style={styles.container}> 
+            <Button title="Sign In" onPress={ handleSignIn }/>
+        </View>
+    )
+}
+
+export default SignIn; 
